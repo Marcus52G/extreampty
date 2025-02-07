c71cc6eff348e2c2b5e133cb33633cea64e2518e
Marcus52G <gicharumark52@gmail.com>
2025-02-07 11:44:57 +0300

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,7 @@
+name = input("name:")
+school = input("school:")
+location = input("location:")
+job = input("job:")
+
+madlib = f"My name are {name} and I am a student at {school}. I live in {location} and I want to be a {job} when I grow up."
+print(madlib)
