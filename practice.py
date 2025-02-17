f279d87e23867e2f11c701db14d01464578427f9
Marcus52G <gicharumark52@gmail.com>
2025-02-17 12:29:59 +0300

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,12 @@
+name = input("name:")
+location = input("location:")
+school = input("school:")
+course =input("course:")
+job = input("job:")
+hobby =input("hobby:")
+religion =input("religion:")
+car =("car:")
+future = input("future:")
+
+madlib = f"My name is {name} and I am a student at {school}. I live in {location} and I am taking {course}. I want to be a {job} when I grow up. I love {hobby} and I am a {religion}. I want to own a {car} in the future and I want to {future}."   
+print(madlib)      
